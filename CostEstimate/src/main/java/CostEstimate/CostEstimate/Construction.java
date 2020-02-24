6f9b18bf896e2e371cf0751227bac2d0953cdca7
neellohit <neellohit69@gmail.com>
2020-02-24 12:22:42 +0530

    Cost Estimate Logger v1.0

--- CostEstimate/src/main/java/CostEstimate/CostEstimate/Construction.java
+++ CostEstimate/src/main/java/CostEstimate/CostEstimate/Construction.java
@@ -1,11 +1,17 @@
 package CostEstimate.CostEstimate;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 //class Construction is a subclass of User
 public class Construction extends User {
+	private static final Logger LOGGER =LogManager.getLogger(Construction.class);
 	//array containing price per square feet for all standards
 	private static int construction_cost_persqft[]={1200,1500,1800,2500};
 	// method to calculate and return the total cost
 	public static double cost(){
-		return (automate?(construction_cost_persqft[user_material_standard]+700):construction_cost_persqft[user_material_standard])*area;
+		double final_cost;
+		final_cost=(automate?(construction_cost_persqft[user_material_standard]+700):construction_cost_persqft[user_material_standard])*area;
+	    LOGGER.debug("debug cost is: "+final_cost);
+		return final_cost;
 	}
 }
+	
--- CostEstimate/src/main/java/CostEstimate/CostEstimate/User.java
+++ CostEstimate/src/main/java/CostEstimate/CostEstimate/User.java
@@ -1,12 +1,15 @@
 package CostEstimate.CostEstimate;
 import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class User {
+	private static final Logger LOGGER =LogManager.getLogger(User.class);
 	//Input class variables input material standard,area of house, fully automate option 
 	public static int user_material_standard;
 	public static double area;
 	public static boolean automate;
 	
-	public static String estimate() { 
+	public static void estimate() { 
 	
 	//Initialising  the scanner
 	Scanner sc=new Scanner(System.in);
@@ -15,12 +18,19 @@ public class User {
 	user_material_standard=sc.nextInt();
 	area=sc.nextDouble();
 	automate=sc.nextBoolean();
+	if(automate)
+	LOGGER.warn("Automation charges will prevail");
 	
 	//closing scanner
 	sc.close();
 	
 	new Construction();
-	return "Construction Estimate is INR "+Construction.cost();
-	}
+	LOGGER.info("Construction Estimate is INR "+Construction.cost());
 	
+	}
+	public static void main(String args[])
+	{
+		LOGGER.info("enter input");
+		estimate();
+	}
 }
